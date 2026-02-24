987b4a7d18528197e2e737ff57ccadb5a1e6cd85
LiamViader <viaderliam.dev@gmail.com>
2026-02-24 14:01:39 +0100

    fixed responsive

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -8,33 +8,51 @@ public class ParallaxEffect : MonoBehaviour
 
     private Transform _cameraTransform;
     private Vector3 _previousCameraPos;
-    private float _spriteHeigh, _startPosition;
+    private float _spriteHeight, _startPosition;
 
     void Start()
     {
         _cameraTransform = Camera.main.transform;
+
+        AdaptarAnchoAPantalla();
+
         _previousCameraPos = _cameraTransform.position;
-        _spriteHeigh = GetComponent<SpriteRenderer>().bounds.size.y;
+        _spriteHeight = GetComponent<SpriteRenderer>().bounds.size.y;
         _startPosition = transform.position.y;
     }
-
 
     void LateUpdate()
     {
         float deltaY = (_cameraTransform.position.y - _previousCameraPos.y) * _parallaxMultiplier;
         float moveAmount = _cameraTransform.position.y * (1 - _parallaxMultiplier);
-        transform.Translate(new Vector3(0,deltaY,0));
+
+        transform.Translate(new Vector3(0, deltaY, 0));
         _previousCameraPos = _cameraTransform.position;
 
-        if (moveAmount > _startPosition + _spriteHeigh)
+        if (moveAmount > _startPosition + _spriteHeight)
         {
-            transform.Translate(new Vector3(0,_spriteHeigh,0));
-            _startPosition += _spriteHeigh;
+            transform.Translate(new Vector3(0, _spriteHeight, 0));
+            _startPosition += _spriteHeight;
         }
-        else if (moveAmount< _startPosition - _spriteHeigh)
+        else if (moveAmount < _startPosition - _spriteHeight)
         {
-            transform.Translate(new Vector3(0,-_spriteHeigh,0));
-            _startPosition -= _spriteHeigh;
+            transform.Translate(new Vector3(0, -_spriteHeight, 0));
+            _startPosition -= _spriteHeight;
         }
+    }
+
+    private void AdaptarAnchoAPantalla()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null || sr.sprite == null) return;
+
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        float cameraWidth = cameraHeight * Camera.main.aspect;
+
+        float spriteWidth = sr.sprite.bounds.size.x;
+
+        float scaleFactor = cameraWidth / spriteWidth;
+
+        transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
     }
 }
